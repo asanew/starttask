01fc67f27e6e7e8318d57988683b70417369d4d2
asanew <asanew@bk.ru>
2017-03-10 16:04:40 +0700

    Update task4.rb

--- lesson2/task4.rb
+++ lesson2/task4.rb
@@ -5,7 +5,7 @@ vowels = %w( e y u i o a ) #['e','y','u','i','o','a']
 #('a'..'z').each_with_index { |l,i| letters[l]=i+1 }
 #vowel_letters = letters.select { |k,v| vowels.include? k }
 vowel_letters = {}
-('a'..'z').each.with_index(1) { |l,i| vowel_letters[l]=i if vowels.include? l }
+('a'..'z').each.with_index(1) { |l,i| vowel_letters[l] = i if vowels.include? l }
 puts "Гласные буквы с их номерами: "
 puts vowel_letters
 
