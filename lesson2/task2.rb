78a6974aa4b12efd98906a4b63591ad5febf7c54
asanew <asanew@bk.ru>
2017-03-10 16:11:29 +0700

    Fix for single-liner

--- lesson2/task2.rb
+++ lesson2/task2.rb
@@ -6,12 +6,11 @@ start_val = 10
 end_val = 100
 step = 5
 
-values = (start_val..end_val).step(step).to_a
+# values = (start_val..end_val).step(step).to_a
 #puts "Массив от "+start_val.to_s+" до "+end_val.to_s+" с шагом "+step.to_s+":"
 puts "Массив от #{start_val.to_s} до #{end_val.to_s} с шагом #{step.to_s}:"
 #values.each do |val|
 #   print val.to_s+" "
 #end
 #puts
-puts values*' '
-
+puts (start_val..end_val).step(step).to_a * ' '
