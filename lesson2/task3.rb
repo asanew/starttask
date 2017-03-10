6da0a134239a87961ae1917c1b6efa78f557fdcd
Asanov Sergey <asanew@bk.ru>
2017-03-10 22:49:24 +0700

    Remarks fixed

--- lesson2/task3.rb
+++ lesson2/task3.rb
@@ -3,13 +3,20 @@
 limit = 100
 
 fib=[1,1]
-loop do
-  next_num = fib[-1]+fib[-2]
-  if next_num > 100
-    break
-  end
+next_num = 2
+while next_num <= limit do
   fib << next_num
+  next_num = fib[-1]+fib[-2]
 end
+
+
+#loop do
+#  next_num = fib[-1]+fib[-2]
+#  if next_num > 100
+#    break
+#  end
+#  fib << next_num
+#end
 puts "Числа Фибонначи до #{limit}:"
 puts fib
 
